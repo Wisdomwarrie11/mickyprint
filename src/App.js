3422ce6f903822dc7de3853a7f6d93eb3747cd14
Wisdomwarrie11 <wisdomwarrie11@gmail.com>
2024-06-07 21:16:04 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Navbar from './Navbar'
 function App (){
   return(
     <>
-    <div style={{backgroundColor: '#FFF6F6'}} >
-    <Navbar/>
-    <Home/>
-    </div>
+   
+      <div style={{backgroundColor: '#FFF6F6'}} className="mw-100 container-lg">
+        <div className="row">
+          <div className="col">
+          <Navbar/>
+          <Home/>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
